61ff48a7d1920d0e7a2ef7755b32546cb439347d
ShuhangFeng <f625227857@gmail.com>
2020-06-21 18:53:02 -0400

    import build from react

---
+++ src/main/webapp/precache-manifest.2e218b0513f83ac200f449419096611c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8df178971e0da6afed740f2eebc70dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d46d6ab3f664b11c51b",
+    "url": "/static/css/2.09d1976d.chunk.css"
+  },
+  {
+    "revision": "2d8058d3d863a41d9573",
+    "url": "/static/css/main.cf0e7b46.chunk.css"
+  },
+  {
+    "revision": "6d46d6ab3f664b11c51b",
+    "url": "/static/js/2.010f03ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.010f03ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d8058d3d863a41d9573",
+    "url": "/static/js/main.67aa69ef.chunk.js"
+  },
+  {
+    "revision": "fba69d4032ae2e2a3194",
+    "url": "/static/js/runtime-main.64ecc692.js"
+  }
+]);
---
+++ src/main/webapp/precache-manifest.3a3d447003194e75cb17c1923924103d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed8d57971e5d74ed907e0a021413b37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d46d6ab3f664b11c51b",
+    "url": "/static/css/2.09d1976d.chunk.css"
+  },
+  {
+    "revision": "a21f60ca88b160ff6f58",
+    "url": "/static/css/main.cf0e7b46.chunk.css"
+  },
+  {
+    "revision": "6d46d6ab3f664b11c51b",
+    "url": "/static/js/2.010f03ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.010f03ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21f60ca88b160ff6f58",
+    "url": "/static/js/main.e39849ea.chunk.js"
+  },
+  {
+    "revision": "fba69d4032ae2e2a3194",
+    "url": "/static/js/runtime-main.64ecc692.js"
+  }
+]);
